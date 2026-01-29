723ee01f1d2bf039502813e33aa4b79ce38bd1d8
Rowell <lr.rverdejo@mmdc.mcl.edu.ph>
2026-01-29 18:58:07 +0800

    Implemented the Scanner class to accomodate user input

--- src/main/java/motorph/Main.java
+++ src/main/java/motorph/Main.java
@@ -3,10 +3,16 @@ package motorph;
 import employee.Employee;
 import enums.EmploymentStatus;
 
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the following employee details:");
+        System.out.println("Employee Number: ");
+        int empNum = sc.nextInt();
 
-        Employee owner = new Employee(10001, "Manuel", "Garcia", "III", "10/11/1983", "Valero Carpark Building Valero Street 1227, Makati City", "966-860-270", "44-4506057-3", "820126853951", "691295330870", "442-605-657-000", EmploymentStatus.REGULAR, "Chief Executive Officer", "N/A");
+        Employee owner = new Employee(empNum, "Manuel", "Garcia", "III", "10/11/1983", "Valero Carpark Building Valero Street 1227, Makati City", "966-860-270", "44-4506057-3", "820126853951", "691295330870", "442-605-657-000", EmploymentStatus.REGULAR, "Chief Executive Officer", "N/A");
 
         System.out.println(owner.getFullName());
         System.out.println(EmploymentStatus.REGULAR);
