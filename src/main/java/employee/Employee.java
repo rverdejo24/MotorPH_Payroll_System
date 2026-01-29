a89ecf7b5f57511f82d31073301fbaba93ac51f7
Rowell <lr.rverdejo@mmdc.mcl.edu.ph>
2026-01-29 18:36:45 +0800

    Add attributes to employee class and create employee instances in the main class

--- src/main/java/employee/Employee.java
+++ src/main/java/employee/Employee.java
@@ -6,14 +6,81 @@ public class Employee {
     private int employeeNumber;
     private String firstName;
     private String lastName;
+    private String suffix;
     private String birthDay;
     private String address;
-    private int phoneNumber;
-    private int sss;
-    private int philHealth;
-    private int pagIbig;
-    private int tinNumber;
+    private String phoneNumber;
+    private String sss;
+    private String philHealth;
+    private String pagIbig;
+    private String tinNumber;
     private EmploymentStatus status;
+    private String position;
+    private String immediateSupervisor;
     
-    
+    public Employee(int employeeNumber, String firstName, String lastName, String suffix, String birthDay, String address, String phoneNumber, String sss, String philHealth, String pagIbig, String tinNumber, EmploymentStatus status, String position, String immediateSupervisor) {
+        this.employeeNumber = employeeNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.sss = sss;
+        this.philHealth = philHealth;
+        this.pagIbig = pagIbig;
+        this.tinNumber = tinNumber;
+        this.status = status;
+        this.position = position;
+        this.immediateSupervisor = immediateSupervisor;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getFullName() {
+        return suffix != null  && !suffix.isEmpty() ? firstName + " " + lastName + " " + suffix : firstName + " " + lastName;
+    }
+
+//    Need to refactor later to convert into date
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getSSS() {
+        return sss;
+    }
+
+    public String getPhilHealth() {
+        return philHealth;
+    }
+
+    public String getPagIbig() {
+        return pagIbig;
+    }
+
+    public String getTinNumber() {
+        return tinNumber;
+    }
+
+    public EmploymentStatus getStatus() {
+        return status;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getImmediateSupervisor() {
+        return immediateSupervisor;
+    }
 }
--- src/main/java/motorph/Main.java
+++ src/main/java/motorph/Main.java
@@ -1,7 +1,13 @@
 package motorph;
 
+import employee.Employee;
+import enums.EmploymentStatus;
+
 public class Main {
     static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Employee owner = new Employee(10001, "Manuel", "Garcia", "III", "10/11/1983", "Valero Carpark Building Valero Street 1227, Makati City", "966-860-270", "44-4506057-3", "820126853951", "691295330870", "442-605-657-000", EmploymentStatus.REGULAR, "Chief Executive Officer", "N/A");
+
+        System.out.println(owner.getFullName());
     }
 }
